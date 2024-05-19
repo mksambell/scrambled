4079d50b37b02a2bab4a0dca360005fe1ab0baef
mksambell <mksambell@gmail.com>
2024-05-19 16:44:03 +0000

    consolidate html into index.html, remove gameplay page, add js file and functions for new, start and end game buttons

---
+++ assets/js/script.js
@@ -0,0 +1,92 @@
+function newGame() {
+
+    // replaces description and logo on landing page with gameplay section
+    let gameplaySection = document.getElementById("gameplay-container");
+    gameplaySection.innerHTML = `
+    <div class="row">
+            <div id="user-input-column" class="col-sm-5 col-10 mx-auto">
+                <div class="row">
+                    <div id="guess-box" class="col-12 text-center">
+                        <label for="guess">Guess:</label>
+                        <input id="guess" type="text" name="guess">
+                    </div>
+                </div>
+                <div class="row">
+                    <div id="lives" class="col-12 text-center">
+                        <i class="fa-solid fa-heart life"></i>
+                        <i class="fa-solid fa-heart life"></i>
+                        <i class="fa-solid fa-heart life"></i>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <button>shuffle</button>
+                        <button>reveal</button>
+                    </div>
+                </div>
+            </div>
+            <div id="feedback-column" class="col-sm-5 col-10 mx-auto">
+                Click 'how to play' to learn the game rules
+                
+                Click 'start game' to display the first anagram
+            </div>
+        </div>
+    `;
+
+    //changes button name and id from 'new game' to 'start game'
+    let buttonDiv = document.getElementById('game-button-div');
+    buttonDiv.innerHTML = `
+    <button id="start-game-button">
+                    start game
+                </button>
+    `;
+
+    //adds event listener to 'start game' button
+    let startGameButton = document.getElementById('start-game-button');
+    startGameButton.addEventListener('click', startGame);
+
+    // displays GOOD LUCK in anagram display
+    let anagram = document.getElementById('anagram');
+    anagram.innerHTML = 'good luck';
+}
+
+function startGame() {
+    //changes button name and id from 'start game' to 'end game'
+    let buttonDiv = document.getElementById('game-button-div');
+    buttonDiv.innerHTML = `
+    <button id="end-game-button">
+                    end game
+                </button>
+    `;
+
+    // adds event listener to 'end game' button
+    let endGameButton = document.getElementById('end-game-button');
+    endGameButton.addEventListener('click', endGame);
+}
+
+function endGame() {
+    // checks if user wants to end game
+
+    // displays GAME OVER in anagram display
+    let anagram = document.getElementById('anagram');
+    anagram.innerHTML = 'game over';
+
+    // changes innerHTML of feedback column and/or user input section to show game summary
+
+    // changes name and id of 'end game' button to 'new game'
+    let buttonDiv = document.getElementById('game-button-div');
+    buttonDiv.innerHTML = `
+    <button id="new-game-button">
+                    new game
+                </button>
+    `;
+
+    // adds event listener to 'new game' button
+    let newGameButton = document.getElementById('new-game-button');
+    newGameButton.addEventListener('click', newGame);
+
+}
+
+let newGameButton = document.getElementById("new-game-button");
+newGameButton.addEventListener('click', newGame);
+
